225d1ccbfd7e2a5fdcb4523d18bb8ec176df3a0d
Bramcoen <coenders123@gmail.com>
2020-09-29 10:03:53 +0200

    Project files - spring boot

---
+++ src/main/java/com/scrumble/boardapi/BoardapiApplication.java
@@ -0,0 +1,13 @@
+package com.scrumble.boardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardapiApplication.class, args);
+	}
+
+}
