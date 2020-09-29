3f475bf6a919e82a4137640bd8a161043004688a
Matthijs <47416015+Matthijsvanspelde@users.noreply.github.com>
2020-09-29 14:16:03 +0200

    Update

--- src/main/java/com/scrumble/boardapi/BoardList.java
+++ src/main/java/com/scrumble/boardapi/BoardList.java
@@ -1,8 +1,6 @@
 package com.scrumble.boardapi;
 
 public class BoardList {
-<<<<<<< HEAD
-=======
     private String name;
     private String description;
     private boolean archived;
@@ -26,5 +24,4 @@ public class BoardList {
     public Task[] getStories() throws Exception {
         throw new Exception("Not implemented");
     }
->>>>>>> 187a464a3e4fa7332559cd6b67fc9d62d84d5c18
 }
