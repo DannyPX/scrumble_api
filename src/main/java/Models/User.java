b197b5875a9d7eecd6a7ee2b374c00401acf15be
Matthijs <47416015+Matthijsvanspelde@users.noreply.github.com>
2020-10-06 14:14:20 +0200

    Fix Bram zijn shit

--- src/main/java/Models/User.java
+++ src/main/java/Models/User.java
@@ -1,20 +1,12 @@
 package Models;
 
-import Models.Enums.Role;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
-@Table
+@Table(name = "[User]")
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private int id;
 
     @Column
--- src/main/java/com/scrumble/boardapi/BoardapiApplication.java
+++ src/main/java/com/scrumble/boardapi/BoardapiApplication.java
@@ -2,8 +2,11 @@ package com.scrumble.boardapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan("Models")
 @SpringBootApplication
+
 public class BoardapiApplication {
 
 	public static void main(String[] args) {
