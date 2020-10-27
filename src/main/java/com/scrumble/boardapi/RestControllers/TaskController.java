83cb24d943ed6f81a5c1c74508e1ce5cce291965
Jasper van der Zwaan <jasper.vanderzwaan.dev@gmail.com>
2020-10-27 11:07:28 +0100

    Added swagger documentation

--- src/main/java/com/scrumble/boardapi/RestControllers/TaskController.java
+++ src/main/java/com/scrumble/boardapi/RestControllers/TaskController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
 @CrossOrigin(origins = {"*"})
 public class TaskController {
 
