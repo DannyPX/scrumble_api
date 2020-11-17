d21ff9429d5a1c1bc0cc5564597b3641069a05ca
Bramcoen <coenders123@gmail.com>
2020-11-17 10:24:59 +0100

    Update Authentication

--- src/main/java/com/scrumble/boardapi/Connections/ApiClient.java
+++ src/main/java/com/scrumble/boardapi/Connections/ApiClient.java
@@ -9,6 +9,7 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class ApiClient {
     private ApiClient() {
@@ -47,5 +48,38 @@ public class ApiClient {
             System.out.println(ex.getMessage());
         }
         return response.get();
+    }
+
+    public static String getUsernameFromToken(String token) {
+        AtomicReference response = new AtomicReference("");
+        JsonObject jsonObject1 = new JsonObject();
+        jsonObject1.addProperty("token", token);
+
+        String sendData = jsonObject1.toString();
+        try {
+            URL url = new URL("http://localhost:8888/getusernamefromtoken");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setDoOutput(true);
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setRequestProperty("Content-Type", "application/json");
+            try (OutputStream outputStream = httpURLConnection.getOutputStream()) {
+                outputStream.write(sendData.getBytes());
+                outputStream.flush();
+            }
+            if (httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()))) {
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        Gson gson = new Gson();
+                        response.set(gson.fromJson(line, String.class));
+                    }
+                }
+            } else {
+                System.out.println("Something went wrong while connecting to API");
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return response.get().toString();
     }
 }
--- src/main/java/com/scrumble/boardapi/Models/User.java
+++ src/main/java/com/scrumble/boardapi/Models/User.java
@@ -12,11 +12,6 @@ public class User {
     @Column
     private String username;
 
-    @Column
-    private String email;
-
-    @Column
-    private String password;
 
     public int getId() {
         return id;
@@ -30,46 +25,18 @@ public class User {
         this.username = username;
     }
 
-    public String getEmail() {
-        return email;
-    }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
     public static class Builder {
         private final String username;
-        private String email;
-        private String password;
 
         public Builder(String username) {
             this.username = username;
         }
 
-        public Builder email(String email) {
-            this.email = email;
-            return this;
-        }
-
-        public Builder password(String password) {
-            this.password = password;
-            return this;
-        }
-
         public User build() {
             User user = new User();
             user.setUsername(username);
-            user.setEmail(email);
-            user.setPassword(password);
             return user;
         }
     }
